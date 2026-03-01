85d9915a63ecef69b8f27c078b71c79c1e46c593
00017159 <shohjahon.sadullayev@icloud.com>
2026-03-02 01:45:03 +0500

    Added CRUD tasks

--- core/views.py
+++ core/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Project, Task, Tag
+
+
+def home(request):
+    return render(request, "core/home.html", {
+        "project_count": Project.objects.count(),
+        "task_count": Task.objects.count(),
+        "tag_count": Tag.objects.count(),
+        "recent_tasks": Task.objects.order_by("-created_at")[:5],
+    })
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "core/task_list.html"
+    context_object_name = "tasks"
+    ordering = ["-created_at"]
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "core/task_detail.html"
+    context_object_name = "task"
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "core/task_form.html"
+    fields = ["project", "title", "description", "assigned_to", "tags", "status"]
+    success_url = reverse_lazy("task_list")
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = "core/task_form.html"
+    fields = ["project", "title", "description", "assigned_to", "tags", "status"]
+    success_url = reverse_lazy("task_list")
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = "core/task_confirm_delete.html"
+    success_url = reverse_lazy("task_list")
